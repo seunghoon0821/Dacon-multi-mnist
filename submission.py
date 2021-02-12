484719e1bbf3e90b2437732f5518c6758467d91d
DK <dongkyuk@andrew.cmu.edu>
2021-02-12 20:01:04 +0900

    add comments

--- submission.py
+++ submission.py
@@ -10,23 +10,22 @@ from dataset import MnistDataset
 from preprocess import transforms_train, transforms_test
 from tta import TTA
 
-# Set device
+# Set Device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Get Model
 model = MnistModel()
 model.load_state_dict(torch.load('data/best_state.pth', map_location=device))
 print(summary(model, input_size=(1, 3, 256, 256), verbose=0))
-
-submit = pd.read_csv('data/sample_submission.csv')
-
 model.eval()
 
+# Prepare Data
+submit = pd.read_csv('data/sample_submission.csv')
 testset = MnistDataset(
     'data/test', 'data/sample_submission.csv', transforms_test)
 test_loader = DataLoader(testset, batch_size=32, num_workers=4)
 
-
+# Test time augmentation
 conf = '{"augs":["NO",\
                 "FLIP_LR",\
                 "FLIP_UD",\
@@ -34,10 +33,10 @@ conf = '{"augs":["NO",\
                 "ROT180",\
                 "ROT270"],\
         "mean":"ARITH"}'
-
-
 model = TTA(model, device, conf)
 
+
+# Inference
 batch_size = test_loader.batch_size
 batch_index = 0
 for i, (images, targets) in enumerate(test_loader):
@@ -51,4 +50,5 @@ for i, (images, targets) in enumerate(test_loader):
     submit.iloc[batch_index:batch_index+batch_size, 1:] = \
         outputs.long().squeeze(0).detach().cpu().numpy()
 
+# Make submission file
 submit.to_csv('data/submit.csv', index=False)
--- train.py
+++ train.py
@@ -8,28 +8,28 @@ from torchinfo import summary
 import torch.optim as optim
 import neptune
 
+# Init Neptune
 neptune.init(project_qualified_name='dongkyuk/dacon-mnist',
              api_token='eyJhcGlfYWRkcmVzcyI6Imh0dHBzOi8vdWkubmVwdHVuZS5haSIsImFwaV91cmwiOiJodHRwczovL3VpLm5lcHR1bmUuYWkiLCJhcGlfa2V5IjoiMTlmOGExYWUtNDRlOS00MTk1LThiOTQtOGY4MDkyZDAxZjY2In0=',
              )
 
 neptune.create_experiment()
 
-
+# Prepare Data
 trainset = MnistDataset(
     'data/train', 'data/dirty_mnist_2nd_answer.csv', transforms_train)
-
-
 train_loader = DataLoader(trainset, batch_size=32, num_workers=8)
 
-print(torch.cuda.is_available())
-
+# Prepare Model
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = MnistModel().to(device)
 print(summary(model, input_size=(1, 3, 256, 256), verbose=0))
 
+# Optimizer, loss
 optimizer = optim.Adam(model.parameters(), lr=1e-3)
 criterion = nn.MultiLabelSoftMarginLoss()
 
+# Train
 num_epochs = 10
 model.train()
 
@@ -46,6 +46,7 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()
 
+        # Log and save
         if (i+1) % 10 == 0:
             outputs = outputs > 0.5
             acc = (outputs == targets).float().mean()
